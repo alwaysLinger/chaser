e0cd5847e9ba4bdd8c535e91cbc53f9ec80fe8cf
yylh <939758149@qq.com>
2022-03-11 13:42:29 +0800

    update examples

--- src/Commands/StartCommand.php
+++ src/Commands/StartCommand.php
@@ -22,33 +22,14 @@ class StartCommand extends Command
     {
         $server = new Server($this->getApplication()->app, 'tcp');
         $server->on('connect', function (Server $server, TcpConnection $connection) use ($output) {
-            // $aid = Timer::after(1, function ($timerId, $args) {
+            // $tid = Timer::tick(1.0, function ($timerId, $args) {
             //     var_dump($timerId, $args);
-            // }, ['after678678']);
-            // $id  = Timer::tick(0.5, function ($timerId, $args) {
-            //     var_dump($timerId, $args);
-            // }, ['tick55']);
-            // $id = Timer::tick(1, function ($timerId, $args) {
-            //     var_dump($timerId, $args);
-            // }, ['tick1']);
-            // $aid = Timer::after(1, function ($timerId, $args) use ($id) {
-            //     var_dump($timerId, $args);
-            //     Timer::clear(3);
-            //     Timer::clearAll();
-            // }, ['after123']);
-            // $aid = Timer::after(3, function ($timerId, $args) {
-            //     var_dump($timerId, $args);
-            //     Timer::clearAll();
-            // }, ['after']);
-            // var_dump('afterid:' . $aid);
-            $tid = Timer::tick(1.0, function ($timerId, $args) {
-                var_dump($timerId, $args);
-            }, ['test']);
-
-            Timer::after(3, function () use ($tid) {
-                var_dump('just once');
-                Timer::clear($tid);
-            });
+            // }, ['test']);
+            //
+            // Timer::after(3, function () use ($tid) {
+            //     var_dump('just once');
+            //     Timer::clear($tid);
+            // });
         });
 
         $server->on('receive', function (Server $server, TcpConnection $conn, string $data) {
